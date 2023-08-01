c38153b58c97f60587de00bb5bb935ea0c1d3558
ywx <1738441128@qq.com>
2023-08-01 17:47:13 +0800

    delete the use of matlab_sym for derivation

--- cal_clf.m
+++ cal_clf.m
@@ -2,16 +2,14 @@ function [V,LV] = cal_clf(x_state)
 %需要确定Vx并写在该函数内部
 %输入为状态变量
 %输出是当前状态下的LV行向量 V标量
-    syms x y real
     D=[20;20];
-    q=[x y]';
-    clf_sym=(q(1)-D(1))^2+(q(2)-D(2))^2;
+    V_func=@(x,y)(x-D(1))^2+(y-D(2))^2;
+    V=V_func(x_state(1),x_state(2));
+
+    LV1=@(x,y)2*(x-D(1));
+    LV2=@(x,y)2*(y-D(2));
+    LV3=0;
     
-    LV_sym=simplify(jacobian(clf_sym,q));
-    LV_func=matlabFunction(LV_sym,'vars',q);
+    LV=[LV1(x_state(1),x_state(2)),LV2(x_state(1),x_state(2)),0];
     
-    LV=LV_func(x_state(1),x_state(2));
-    
-    clf_func=matlabFunction(clf_sym,'vars',q);
-    V=clf_func(x_state(1),x_state(2));
 end
