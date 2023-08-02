48e0bdaa518b15ff5f2defc47a0e4f9f1f12af6f
ywx <1738441128@qq.com>
2023-08-02 10:49:34 +0800

    HOCBF m=2 code

--- HOCBF_2Dmove/cal_cbf.m
+++ HOCBF_2Dmove/cal_cbf.m
@@ -7,11 +7,10 @@ function [h,Lfh,Lf2h,LgLfh] = cal_cbf(x_state)
     v=x_state(3);
     theta=x_state(4);
     
-    X=[10;12];Xr=3;
+    X=[20;1];Xr=4;
     h=(x-X(1))^2+(y-X(2))^2-Xr^2;
     
     Lfh=2*(x-X(1))*v*cos(theta)+2*(y-X(2))*v*sin(theta);
     Lf2h=2*v^2;
-    LgLfh=[2*(x-X(1))*cos(theta)+2*(y-X(2))*sin(theta),
-        2*(y-X(2))*v*cos(theta)-2*(x-X(1)*v*sin(theta)];
+    LgLfh=[2*(x-X(1))*cos(theta)+2*(y-X(2))*sin(theta),2*(y-X(2))*v*cos(theta)-2*(x-X(1))*v*sin(theta)];
 end
--- HOCBF_2Dmove/cal_clf.m
+++ HOCBF_2Dmove/cal_clf.m
@@ -2,8 +2,8 @@ function [V1,V2,LfV1,LgV1,LfV2,LgV2] = cal_clf(x_state)
 %需要确定Vx并写在该函数内部
 %输入为状态变量
 %输出是当前状态下的LV行向量 V标量
-    D=[20;20];  %目标位置
-    vd=1;       %期望速度
+    D=[45;0];  %目标位置
+    vd=2;       %期望速度
     x=x_state(1);
     y=x_state(2);
     v=x_state(3);
@@ -12,7 +12,7 @@ function [V1,V2,LfV1,LgV1,LfV2,LgV2] = cal_clf(x_state)
     
     V1=(theta-atan((D(2)-y)/(D(1)-x)))^2;
     LfV1=2*(atan((D(2)-y)/(D(1)-x))-theta)*(D(2)-y)/((D(1)-x)^2*(((D(2)-y)/(D(1)-x))^2+1))* v*cos(theta)+...
-        2*(atan((D(2)-y)/(D(1)-x))-theta)*(D(2)-y)/((x-X(1))*(((D(2)-y)/(D(1)-x))^2+1))*v*sin(theta);
+        2*(atan((D(2)-y)/(D(1)-x))-theta)*(D(2)-y)/((x-D(1))*(((D(2)-y)/(D(1)-x))^2+1))*v*sin(theta);
     LgV1=[0,2*(theta-atan((D(2)-y)/(D(1)-x)))];
     
     V2=(v-vd)^2;
